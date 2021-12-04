7ce6d9997bb4ee5ae9504f072534e666d2f8b794
chengyx <chengyxnet@163.com>
2021-12-04 15:26:49 +0800

    工具类封装

---
+++ common/src/main/java/exception/BizException.java
@@ -0,0 +1,20 @@
+package exception;
+
+import enums.BizCodeEnum;
+import lombok.Data;
+
+/**
+ * @Description BizException
+ * @Author cyx
+ * @Date 2021/12/4
+ **/
+@Data
+public class BizException extends RuntimeException{
+    private int code;
+    private String msg;
+
+    public BizException(BizCodeEnum bizCodeEnum) {
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+    }
+}
